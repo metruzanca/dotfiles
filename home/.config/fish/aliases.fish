ca814a04a37970d5d47531e5b063e64308d70ce1
Sam Zanca <sam@zanca.dev>
2022-11-25 10:03:24 -0500

    wsl abbr

--- home/.config/fish/aliases.fish
+++ home/.config/fish/aliases.fish
@@ -1,3 +1,9 @@
+# TODO on next windows install, windows username and wsl username should be identical (case sensitive)
+
 # Since alacritty is installed on the host machine
 abbr arc "$EDITOR $c/Users/Metru/AppData/Roaming/alacritty/alacritty.yml"
 abbr rc "$EDITOR $c/Users/Metru/dev/dotfiles"
+
+if test $isWSL
+  abbr espanso "$EDITOR $c/Users/Metru/AppData/Roaming/espanso/match/base.yml"
+end
