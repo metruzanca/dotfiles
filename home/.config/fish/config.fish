a32b90054adb8b7c83b951a3f190d9cf71192bc9
Sam Zanca <sam@zanca.dev>
2022-12-24 11:16:53 -0500

    Added hook for running lsd after cd

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,3 +5,20 @@ source ~/.config/fish/3_init.fish
 function fish_greeting
   macchina
 end
+
+
+if type -q lsd
+  # See https://github.com/rstacruz/fish-autols/ but changed to use lsd instead
+  function __autols_hook --description "Auto ls" --on-event fish_prompt
+    # if test "$NO_AUTO_LS" != ""
+    #   return
+    # end
+
+    if test "$__autols_last" != (pwd)
+      lsd
+    end
+    set  -g __autols_last (pwd)
+  end
+end
+
+
