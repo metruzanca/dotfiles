3c9f3f3090bde45bc42f261944117870ff46d076
Sam Zanca <sam@zanca.dev>
2022-12-29 10:12:37 -0500

    Installer script

---
+++ installers/mint.sh
@@ -0,0 +1,7 @@
+# Required for Brew
+sudo apt-get install --yes git build-essential procps curl file
+
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Setup Brew
