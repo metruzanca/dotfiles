8e4e74e4fcd7df573423ec5b69f1aa124b24ee44
Sam Zanca <sam@zanca.dev>
2023-04-18 15:52:34 -0400

    new global function

--- home/.config/zsh/aliases.sh
+++ home/.config/zsh/aliases.sh
@@ -4,3 +4,17 @@ alias ls=lsd
 cd() { builtin cd "$@" && ls; }
 # Like pbcopy but without the trailing new-line
 alias clipboard="tr -d '\n' | pbcopy"
+
+jira-branch() {
+    if [ -z "$1" ]; then
+        echo "Usage: jira-branch <jira url> [dash-spaced-description]"
+        return 1
+    fi
+    branch_name=$(echo "$1" | awk -F'/' '{print $NF}')
+    if [ -n "$2" ]; then
+        branch_name="$branch_name-$2"
+    fi
+    git checkout -b "$branch_name"
+}
+
+alias dot='$EDITOR $HOME/dotfiles'
