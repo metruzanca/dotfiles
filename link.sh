7750f156d327451c774a97770e914cf82eea1469
Sam Zanca <sam@zanca.dev>
2023-04-18 15:59:00 -0400

    small refactor

--- link.sh
+++ link.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-if [ -x "$(command -v espanso)" ]; then 
-  stow home;
-  
-  if [ -d "private" ]; then
-    stow private;
-  fi
-else
+if [ ! -x "$(command -v stow)" ]; then
   echo "GNU stow not installed".
+  return 0;
 fi
 
 if [ -x "$(command -v espanso)" ]; then 
   espanso restart
 fi
+
+try_stow() {
+  if [ -d "$1" ]; then
+    stow "$1";
+  fi
+}
+
+stow home;
+try_stow "private";
