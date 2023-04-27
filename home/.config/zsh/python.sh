fc1b9219577994cdfe6c57e8c04b3089f25b745b
Sam Zanca <samuele@zanca.dev>
2023-04-27 10:54:42 -0400

    simplified python setup

--- home/.config/zsh/python.sh
+++ home/.config/zsh/python.sh
@@ -1,44 +1,10 @@
 #!/usr/bin/env bash
 
-alias pm="python3 manage.py"
-
-# Python Related
-venv-install() {
-    deactivate &> /dev/null
-    # if no virtual environment exists, create one
-    if [[ ! -d .venv ]]; then
-        python3 -m venv .venv
-    fi
-    # activate the virtual environment
-    source .venv/bin/activate
-    # install the requirements
-    pip install -r requirements.txt
-}
-
-# Venv is super inconsistent for some reason. sigh.
-
-venv() {
-    # Deactivate the venv if the cwd is not a subdirectory of the venv
-    # if [ -v "$VIRTUAL_ENV" ]; then
-    #     prefix=$(dirname "$VIRTUAL_ENV");
-    #     echo "$(pwd) $prefix";
-    #     if [[ "$(pwd)" != "$prefix"* ]]; then
-    #         deactivate &> /dev/null
-    #     fi
-    # fi
-    # check if a .venv directory exists
-    if [[ -d .venv ]]; then
-        source .venv/bin/activate 
-    fi
-}
-
-cd() {
-    # do everything normal `cd` does
-    builtin cd "$@" || return 
-    # list the contents of the directory
-    ls;
-    # activate the virtual environment if it exists
-    venv;
-}
-
-venv;
+# If no venv, create one
+alias venv-create='[[ ! -d .venv ]] && python3 -m venv .venv'
+# if venv, activate it
+alias venv-activate='[[ -d .venv ]] && source .venv/bin/activate'
+# If we're in a venv, install requirements
+alias venv-install='[ -n "$VIRTUAL_ENV" ] && pip install -r requirements.txt'
+# Convenience alias
+alias pm='python3 manage.py'
