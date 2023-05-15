86c7449ebdfd2d1a44ee96f2d0a97d9e6002056f
Sam Zanca <samuele@zanca.dev>
2023-05-15 15:42:53 -0400

    clean checkout + bash sheet

--- home/.config/zsh/aliases.sh
+++ home/.config/zsh/aliases.sh
@@ -3,9 +3,9 @@
 
 alias ls=lsd -a
 cd() { builtin cd "$@" && ls; }
-# Like pbcopy but without the trailing new-line
-alias clipboard="tr -d '\n' | pbcopy"
-alias trim="sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'"
+
+# Cool, it works! But I don't think I'll need it.
+# rgb() { printf "\033[38;2;${1:-0};${2:-0};${3:-0}m"; } # 8-bit color
 
 jira() {
     local command_name='jira'
@@ -26,6 +26,9 @@ jira() {
 
     branch_name=$(echo "$1" | awk -F'/' '{print $NF}')
 
+    # Function to remember what this does
+    function trim { sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'; }
+
     # If a branch for that ticket already exists, check it out
     local existing=$(git branch | grep -m 1 "$branch_name" | trim)
     if [ -n "$existing" ]; then
@@ -42,3 +45,24 @@ jira() {
 }
 
 alias dot='$EDITOR $HOME/dotfiles'
+
+git-checkout-clean() {
+    local reset=$(tput sgr0)
+    local red=$(tput setaf 1)
+    local green=$(tput setaf 2)
+    local pink=$(tput setaf 5)
+    local bold=$(tput bold)
+    if [ -z "$1" ]; then
+        printf "${red}Error: No argument provided.${reset}\n\n";
+        printf "Usage: git-checkout-clean <branch-name> - deletes the branch locally and checks out the remote version.\n";
+        return 1;
+    fi
+    
+    git checkout "$(git master)" &>/dev/null;
+    git branch -D "$1" &>/dev/null;
+    git checkout "$1" &>/dev/null;
+
+    if [ "$(git rev-parse --abbrev-ref HEAD)" = "staging" ]; then
+        printf "Checked out ${green}${bold}fresh${reset} version of ${pink}${bold}${1}${reset} from remote.\n"
+    fi
+}
