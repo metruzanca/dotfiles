5a715b684ac466ee81d5f734fb1077f4e9b531c4
Sam Zanca <samuele@zanca.dev>
2023-05-31 18:42:01 -0400

    2023-05-31T18:42:01.096Z

--- home/.config/zsh/aliases.sh
+++ home/.config/zsh/aliases.sh
@@ -7,3 +7,6 @@ cd() { builtin cd "$@" && ls; }
 
 # Cool, it works! But I don't think I'll need it.
 # rgb() { printf "\033[38;2;${1:-0};${2:-0};${3:-0}m"; } # 8-bit color
+function calc {
+    echo "$@" | bc
+}
