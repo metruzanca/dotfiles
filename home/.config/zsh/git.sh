15285b6d93783ff388ad75de7dfa58358f200dd8
Sam Zanca <samuele@zanca.dev>
2023-09-11 13:23:49 -0400

    2023-09-11T13:23:47.814Z

--- home/.config/zsh/git.sh
+++ home/.config/zsh/git.sh
@@ -50,7 +50,6 @@ git-checkout-clean() {
         return 1;
     fi
     
-    git stash;
     git checkout "$(git master)" &>/dev/null;
     git branch -D "$1" &>/dev/null;
     git checkout "$1" &>/dev/null;
