7d60c93f69a2b2e84f7b85351eb672d4f28a02b1
Sam Zanca <samuele@zanca.dev>
2023-10-05 11:30:52 -0400

    updates to fresh and stage

--- home/.config/git/fresh.sh
+++ home/.config/git/fresh.sh
@@ -16,9 +16,12 @@ fi
 
 target="$1"
 
+# A fucking lambda in bash mfs. Sick.
+current_branch() { git rev-parse --abbrev-ref HEAD; }
+
 # if current branch is target, checkout master. This is needed because you get
 # an error if you try to delete a branch you're currently on
-if [ "$(git rev-parse --abbrev-ref HEAD)" = "$target" ]; then
+if [ "$(current_branch)" = "$target" ]; then
   master="$(git remote show origin | grep 'HEAD branch' | cut -d' ' -f5)"
   git checkout "$master" &>/dev/null;
 fi
@@ -27,6 +30,6 @@ git branch -D "$target" &>/dev/null;
 git checkout "$target" &>/dev/null;
 
 # Generally when this command fails, it endes up on master and not $1.
-if [ "$(git rev-parse --abbrev-ref HEAD)" = "$target" ]; then
+if [ "$(current_branch)" = "$target" ]; then
   printf "Checked out a ${green}${bold}fresh${reset} version of ${pink}${bold}${target}${reset} from remote.\n"
 fi
