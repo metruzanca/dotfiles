a03327431487aeab45fa7201fefc52567b85c69a
Sam Zanca <samuele@zanca.dev>
2024-03-16 20:49:39 -0400

    wip config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,9 +1,7 @@
 # ---------- Variables ----------
-export nvm_default_version="v16.18.1"
-export EDITOR="code-insiders"
+# export nvm_default_version="v16.18.1"
+export EDITOR="code"
 export C="/mnt/c"
-
-# source $HOME/.config/fish/env.fish
 
 if type -q bass
     bass source $HOME/.config/fish/functions.sh
@@ -12,55 +10,14 @@ end
 
 # export USERWINDOWS=(powershell.exe '$env:UserName')
 
-# ---------- Aliases ----------
-
-# TODO on next windows install, windows username and
-# wsl username should be identical (case sensitive)
-
-# Since alacritty is installed on the host machine
-abbr c "$EDITOR"
-abbr w "cd /mnt/c/Users/samue/"
-
-# Projects
-abbr dot "$EDITOR ~/dev/dotfiles"
-abbr z "$EDITOR /mnt/c/Users/samue/Documents/zanca.dev-quartz"
-
 if test $isWSL
     abbr winget "winget.exe"
+    abbr w "cd /mnt/c/Users/samue/"
 end
 
 if type -q lsd
-    abbr ls lsd
-end
+    abbr ls lsd;
 
-alias trim="awk '{\$1=\$1;print}'"
-
-# ---------- Initializations ----------
-
-# Rust
-fish_add_path $HOME/.cargo/bin
-# . "$HOME/.cargo/env"
-
-# pnpm
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
-
-# Volta
-# Volta
-
-# Add homebrew to path
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-# ---------- Fish Shell configuration ----------
-
-function fish_greeting
-  # This function empty silences the default message
-  # macchina
-end
-
-
-if type -q lsd
     # See https://github.com/rstacruz/fish-autols/ but changed to use lsd instead
     function __autols_hook --description "Auto ls" --on-event fish_prompt
         # if test "$NO_AUTO_LS" != ""
@@ -72,6 +29,27 @@ if type -q lsd
         end
         set -g __autols_last (pwd)
     end
+end
+
+# Rust
+fish_add_path $HOME/.cargo/bin
+# . "$HOME/.cargo/env"
+
+# pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+
+# Volta
+
+# Add homebrew to path
+# eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# ---------- Fish Shell configuration ----------
+
+function fish_greeting
+  # This function empty silences the default message
+  # macchina
 end
 
 # ---- Starship ----
@@ -95,15 +73,15 @@ set -gx GOPATH "$HOME/go"
 set -gx PATH "$GOPATH/bin" $PATH
 
 # SurrealDB
-set -gx PATH "$HOME/.surrealdb" $PATH
+# set -gx PATH "$HOME/.surrealdb" $PATH
 
 # Fly.io
-set -gx FLYCTL_INSTALL "$HOME/.fly"
-set -gx PATH "$FLYCTL_INSTALL/bin" $PATH
+# set -gx FLYCTL_INSTALL "$HOME/.fly"
+# set -gx PATH "$FLYCTL_INSTALL/bin" $PATH
 
 # bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+# set --export BUN_INSTALL "$HOME/.bun"
+# set --export PATH $BUN_INSTALL/bin $PATH
 
-# opam configuration
-source /home/szanca/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
+# Ocaml opam configuration
+# source /home/szanca/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
---
+++ notes.md
@@ -0,0 +1,10 @@
+Changes to MacBook post install:
+- Natural Scrolling on -> off
+- Install brew (which sets up all dev stuff)
+- git clone dotfiles
+- brew install --cask arc spotify ticktick visual-studio-code obsidian alt-tab rectangle iterm3      
+- setup iterm to be dropdown (TODO make a blog post on this)
+- stow dotfiles/macos (for vscode settings)
+- brew install fish
+- make dotfiles branch (to not break my home machine)
+- Hide dock
