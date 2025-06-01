6a4466a13c82017be67e76eb8131a9562c063155
Sam Zanca <sam@zanca.dev>
2025-06-01 17:23:37 -0400

    Got rid of no longer used

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,64 +1,41 @@
-# ---------- Variables ----------
-export nvm_default_version="v16.18.1"
-export EDITOR="nvim"
-export C="/mnt/c"
+# ---------- Initializations ----------
+set -gx EDITOR hx
 
-# source $HOME/.config/fish/env.fish
-
-if type -q bass
-    bass source $HOME/.config/fish/functions.sh
-    bass source $HOME/.config/fish/is_wsl.sh
+if set -q ZELLIJ
+else
+    zellij
 end
 
-# export USERWINDOWS=(powershell.exe '$env:UserName')
+if type -q starship
+    starship init fish | source
+end
 
-# ---------- Aliases ----------
+if type -q zoxide
+    zoxide init fish | source
+end
 
-# TODO on next windows install, windows username and
-# wsl username should be identical (case sensitive)
+if type -q direnv
+    direnv hook fish | source
+end
 
-# Since alacritty is installed on the host machine
-abbr c "$EDITOR"
-abbr w "cd /mnt/c/Users/samue/"
+# Rust
+#fish_add_path $HOME/.cargo/bin
 
-# Projects
-abbr dot "$EDITOR ~/dev/dotfiles"
+# ---------- Fish Shell configuration ----------
 
-if test $isWSL
-    abbr winget "winget.exe"
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+  pfetch
 end
 
 if type -q lsd
     abbr ls lsd
 end
 
-alias trim="awk '{\$1=\$1;print}'"
-
-# ---------- Initializations ----------
-
-# Rust
-fish_add_path $HOME/.cargo/bin
-# . "$HOME/.cargo/env"
-
-# pnpm
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
-
-# Volta
-# Volta
-
-# Add homebrew to path
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-# ---------- Fish Shell configuration ----------
-
-function fish_greeting
-  # This function empty silences the default message
-  # macchina
-end
-
-
+# Auto run ls when cd-ing around
 if type -q lsd
     # See https://github.com/rstacruz/fish-autols/ but changed to use lsd instead
     function __autols_hook --description "Auto ls" --on-event fish_prompt
@@ -71,53 +48,4 @@ if type -q lsd
         end
         set -g __autols_last (pwd)
     end
-end
-
-# ---- Starship ----
-
-if type -q starship
-    starship init fish | source
-end
-
-
-# direnv
-if type -q direnv
-    direnv hook fish | source
-end
-
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-# Golang
-# Update go e.g. https://tecadmin.net/how-to-install-go-on-ubuntu-20-04/
-set -gx GOPATH "$HOME/go"
-set -gx PATH "$GOPATH/bin" $PATH
-
-# SurrealDB
-set -gx PATH "$HOME/.surrealdb" $PATH
-
-# Fly.io
-set -gx FLYCTL_INSTALL "$HOME/.fly"
-set -gx PATH "$FLYCTL_INSTALL/bin" $PATH
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# # Generated for envman. Do not edit.
-test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
-
-
-# Gleam
-abbr gm "gleam run -m"
-
-# zoxide
-if type -q zoxide
-  zoxide init fish | source
-end
-
-
-# WSL
-function ps
-  powershell.exe -Command "$argv"
 end
--- home/.config/zsh/python.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-alias venv-create='python3 -m venv .venv && source .venv/bin/activate'
-alias venv='source .venv/bin/activate'
-# I want to prevent installing globally so I'm checking if the virtual environment is active
-alias venv-install='[ -n "$VIRTUAL_ENV" ] && pip install -r requirements.txt'
-# Convenience alias
-alias pm='python3 manage.py'
