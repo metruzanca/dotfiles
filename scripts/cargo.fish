30a8e4b54422a170452e230c408eb85d3ba1bc93
Sam Zanca <samuele@zanca.dev>
2025-07-07 11:56:21 -0400

    now tracking cargo extensions in dotfiles

---
+++ scripts/cargo.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+# ---------- Script Code ----------
+
+if not type -q cargo; or not type -q gum
+  echo "Cargo and/or gum is not installed."
+  exit 1
+end
+
+
+function confirm_install
+  set -l tool $argv[1]
+
+  # Check if the tool is already installed
+  cargo $tool --version &> /dev/null
+
+  # Status is set to 0 if the command was successful
+  if not test $status -eq 0
+    set -l cargo_pkg "cargo-$tool"
+    # Use gum to confirm installation
+    if gum confirm "Install $cargo_pkg?"; 
+      cargo install $cargo_pkg
+    end
+  end
+end
+
+# ---------- Cargo extensions ----------
+
+confirm_install watch
