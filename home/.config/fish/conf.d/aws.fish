310af7187d2d3feedbbcebdfa1cc338064556ac8
Sam Zanca <samuele@zanca.dev>
2025-07-07 17:59:40 -0400

    wsl now using latest dotfiles

--- home/.config/fish/conf.d/aws.fish
+++ home/.config/fish/conf.d/aws.fish
@@ -1,2 +1,4 @@
 # AWS Cli has no way to get the profile from the command line, so we use grep to extract it from the config file.
-export AWS_PROFILE=(grep -E '^\[profile |^\[default\]' ~/.aws/config | sed 's/^\[profile //;s/^\[default\]/default/;s/\]$//')
+if test -f ~/.aws/config
+    set -x AWS_PROFILE (grep -E '^\[profile |^\[default\]' ~/.aws/config | sed 's/^\[profile //;s/^\[default\]/default/;s/\]$//')
+end
--- home/.config/fish/conf.d/volta.fish
+++ home/.config/fish/conf.d/volta.fish
@@ -2,3 +2,8 @@
 if test (uname) = "Darwin" && type -q volta
     fish_add_path "/Users/(whoami)/.volta/bin"
 end
+
+if test -d "$HOME/.volta/bin"
+    set -gx VOLTA_HOME "$HOME/.volta"
+    set -gx PATH "$VOLTA_HOME/bin" $PATH
+end
