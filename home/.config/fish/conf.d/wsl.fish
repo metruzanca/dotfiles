028a95c52501205ef43453c4d8651751e53d71cd
Sam Zanca <samuele@zanca.dev>
2025-07-09 10:12:45 -0400

    fixed script on other systems

--- home/.config/fish/conf.d/wsl.fish
+++ home/.config/fish/conf.d/wsl.fish
@@ -1,11 +1,13 @@
 #!/usr/bin/env fish
 
-if grep -q microsoft /proc/version
-  # echo "Detected WSL environment"
-  abbr winget "winget.exe"
-  abbr docker "ps docker"
+if test -f /proc/version
+  if grep -q microsoft /proc/version
+    # echo "Detected WSL environment"
+    abbr winget "winget.exe"
+    abbr docker "ps docker"
 
-  function ps
-    powershell.exe -Command "$argv"
+    function ps
+      powershell.exe -Command "$argv"
+    end
   end
 end
