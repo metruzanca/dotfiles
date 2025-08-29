eb95a13b723837ea0c973ca40582006c150b5226
Sam Zanca <samuele@zanca.dev>
2025-08-29 13:09:56 -0400

    oops small bug

--- home/.config/fish/functions/code.fish
+++ home/.config/fish/functions/code.fish
@@ -1,7 +1,7 @@
 function code
-    set -l cmd "code"
+    set -l cmd (which code)
     # This trick opens vscode much faster on macOS
-    if test (uname) = "Darwin"
+    if test (uname) = "Darwin";
         set -l cmd 'open -a "Visual Studio Code"'
     end
 
