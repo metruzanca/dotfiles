b91f1783c3a2a09911e6acb2b7bff5498bed5b14
Sam Zanca <samuele@zanca.dev>
2025-08-29 11:29:57 -0400

    fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,14 +1,13 @@
-# ---------- Utilities ----------
+# ---------- Script Utilities ----------
 set -l os (uname)
 set -l username (whoami)
 # ---------- Fish Shell configuration ----------
 
-# Required on macos as a first step
+# Setup homebrew first as most packages depend on this
 if test -f /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
 end
 
-# Required on if using Linuxbrew
 if test -f /home/linuxbrew/.linuxbrew/bin/brew
   eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
@@ -18,52 +17,25 @@ fish_add_path /opt/homebrew/opt/uutils-coreutils/libexec/uubin
 
 set -gx EDITOR hx
 
+# Shell integrations
+if type -q starship; starship init fish | source; end
+if type -q zoxide; zoxide init fish | source; end
+if type -q direnv; direnv hook fish | source; end
+if type -q zoxide; abbr cd z; end
+
 if type -q zellij
+    abbr zj "zellij attach main || zellij --session main || zellij";
+
     if set -q ZELLIJ
     else
         zellij attach main || zellij --session main || zellij
     end
 end
 
-if type -q starship
-    starship init fish | source
-end
-
-if type -q zoxide
-    zoxide init fish | source
-end
-
-if type -q direnv
-    direnv hook fish | source
-end
-
 if type -q lsd
-    abbr ls lsd
-end
+    abbr ls lsd;
 
-if type -q zoxide
-    abbr cd z
-end
-
-if type -q zellij
-    abbr zj "zellij attach main || zellij --session main || zellij"
-end
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-function fish_greeting
-    if type -q pfetch
-        if test "$TERM_PROGRAM" = "vscode"
-            return
-        end
-        pfetch
-    end
-end
-
-# Auto run ls when cd-ing around
-if type -q lsd
+    # Auto run ls when cd-ing around
     # See https://github.com/rstacruz/fish-autols/ but changed to use lsd instead
     function __autols_hook --description "Auto ls" --on-event fish_prompt
         # if test "$NO_AUTO_LS" != ""
@@ -77,8 +49,14 @@ if type -q lsd
     end
 end
 
-# TODO use rust uutils-coreutils instead of GNU coreutils
-# fish_add_path "/opt/homebrew/opt/uutils-coreutils/libexec/uubin"
+function fish_greeting
+    if type -q pfetch
+        if test "$TERM_PROGRAM" = "vscode"
+            return
+        end
+        pfetch
+    end
+end
 
 # ---------- Auto appended scripts ----------
 
