52f3316505935fe6b62423e9d70071ce5a6f0ff2
Sam Zanca <samuele@zanca.dev>
2025-09-10 14:02:26 -0400

    better fuzzy

--- home/.config/fish/conf.d/fuzzy-find.fish
+++ home/.config/fish/conf.d/fuzzy-find.fish
@@ -1,8 +1,7 @@
-if type -q fzf and type -q bat
+if type -q fzf and type -q bat and type -q fd
   function fzf_find_file
-    command find . -type f -print | fzf --preview 'bat --color=always {}'
+    command fd -t f --hidden --exclude .git . | fzf --preview 'bat --color=always {}'
   end
 
   bind \cp 'fzf_find_file'
 end
-
