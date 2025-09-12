abee816780c0722f5c4a8c78334dbb91426e8298
Sam Zanca <samuele@zanca.dev>
2025-09-12 18:09:10 -0400

    abbr

--- home/.config/fish/conf.d/abbr.fish
+++ home/.config/fish/conf.d/abbr.fish
@@ -1,3 +1,7 @@
 abbr loc 'git ls-files -z | xargs -0 wc -l 2>/dev/null | awk "END {print \$1}" | awk "{printf \"%'\''d LOC\\n\", \$0}"'
 abbr mkdir 'mkdir -p'
 abbr rm 'rm -r'
+
+# Golang
+## I keep writing got when going fast so let's make it an abbreviation
+abbr got "go test ./..."
