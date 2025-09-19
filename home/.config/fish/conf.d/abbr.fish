c9ee634421251831c5f802e098a4d82772358161
Sam Zanca <samuele@zanca.dev>
2025-09-19 13:06:46 -0400

    fuzzy find is awesome

--- home/.config/fish/conf.d/abbr.fish
+++ home/.config/fish/conf.d/abbr.fish
@@ -1,10 +1,15 @@
-abbr loc 'git ls-files -z | xargs -0 wc -l 2>/dev/null | awk "END {print \$1}" | awk "{printf \"%'\''d LOC\\n\", \$0}"'
-abbr git-fuzzy 'git log --color --date=format:"%Y-%m-%d" --format="%h - %cd - %s" | fzf --ansi --preview "git show --color {1}"'
+# Fish shell abbreviations
 
+# Very basic approximation of line of code count in a git repository
+abbr loc 'git ls-files -z | xargs -0 wc -l 2>/dev/null | awk "END {print \$1}" | awk "{printf \"%'\''d LOC\\n\", \$0}"'
+# Cd to the root of the git repository or stay in the current directory if not in a git repo
+abbr cdg 'cd $(git rev-parse --show-toplevel || echo .)'
+
+# Default flags for common commands
 abbr mkdir 'mkdir -p'
 abbr rm 'rm -r'
 
-# Golang
+# Golang specific abbreviations
 if type -q go
   ## I keep writing got when going fast so let's make it an abbreviation
   abbr got "go test ./... | grep -v '?'"
--- home/.config/fish/conf.d/fuzzy-find.fish
+++ home/.config/fish/conf.d/fuzzy-find.fish
@@ -1,7 +1,15 @@
-if type -q fzf and type -q bat and type -q fd
-  function fzf_find_file
-    command fd -t f --hidden --exclude .git . | fzf --preview 'bat --color=always {}'
+if type -q fzf
+
+  if type -q bat and type -q fd
+    # Fuzzy find and open files with a preview using fzf, fd, and bat
+    function fzf_find_file
+      command fd -t f --hidden --exclude .git . | fzf --preview 'bat --color=always {}'
+    end
+
+    # Bind Ctrl+P to the fzf_find_file function
+    bind \cp 'fzf_find_file'
   end
 
-  bind \cp 'fzf_find_file'
+  # Show a fuzzy list of git commits with a preview of the commit diff
+  abbr git-fuzzy 'git log --color --date=format:"%Y-%m-%d" --format="%h - %cd - %s" | fzf --ansi --preview "git show --color {1}"'
 end
