67a9f9a4ea5b5a89dcdb5dcce4966f3cdc6e284b
Sam Zanca <samuele@zanca.dev>
2026-01-05 13:36:50 -0500

    git & fish aliases

--- home/.config/fish/conf.d/abbr.fish
+++ home/.config/fish/conf.d/abbr.fish
@@ -10,3 +10,7 @@ abbr rm 'rm -r'
 # This line breaks syntax highlighting, so its at the bottom to not bother me
 # Very basic approximation of line of code count in a git repository
 abbr loc 'git ls-files -z | xargs -0 wc -l 2>/dev/null | awk "END {print \$1}" | awk "{printf \"%'\''d LOC\\n\", \$0}"'
+
+
+abbr c 'cursor .'
+abbr ca 'cursor-agent'
