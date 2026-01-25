7274e463045996fcb9c6f9e89ec883c4d9006f58
Metru <sam@zanca.dev>
2026-01-25 17:38:00 -0500

    new zellij config

--- home/.config/fish/conf.d/zellij.fish
+++ home/.config/fish/conf.d/zellij.fish
@@ -1,8 +1,19 @@
 if type -q zellij
-  abbr zj "zellij attach main || zellij --session main || zellij";
+  if status is-interactive
+    # Configure auto-attach/exit to your likings (default is off).
+    # set ZELLIJ_AUTO_ATTACH true
+    set ZELLIJ_AUTO_EXIT true
+    # eval (zellij setup --generate-auto-start fish | string collect)
+  end
+  if not set -q ZELLIJ; and test "$TERM_PROGRAM" != "vscode"
+    if test "$ZELLIJ_AUTO_ATTACH" = "true"
+        zellij attach -c
+    else
+        zellij
+    end
 
-  if set -q ZELLIJ
-  else
-    zellij attach main || zellij --session main || zellij
+    if test "$ZELLIJ_AUTO_EXIT" = "true"
+        kill $fish_pid
+    end
   end
 end
