1fc7a66c81cac936e05d21854164331cf7a0f6bd
Zanca <sam@zanca.dev>
2026-02-16 10:40:18 -0500

    zed + figma-mcp

--- home/.config/fish/conf.d/0_init.fish
+++ home/.config/fish/conf.d/0_init.fish
@@ -19,4 +19,4 @@ end
 # ---------- Default Environment Variables ----------
 # This fish file gets loaded first, so we can set default env vars here
 # the $HOME/.config/fish/config.fish file can override these if needed
-set -gx EDITOR hx
+set -gx EDITOR "zed --wait"
