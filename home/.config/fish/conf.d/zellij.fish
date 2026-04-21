062f7672061753f3c4e20bb9b8068bef2ec70dd2
Sam Zanca <sam@zanca.dev>
2026-04-21 12:12:59 -0400

    zellij config

--- home/.config/fish/conf.d/zellij.fish
+++ home/.config/fish/conf.d/zellij.fish
@@ -1,9 +1,3 @@
 if type -q zellij
-  # Start zellij automatically (except in vscode)
-  # if not set -q ZELLIJ; and test "$TERM_PROGRAM" != "vscode"
-  #   # Attach to "main" session or create it if it doesn't exist
-  #   zellij attach --create main
-  #   # kill fish when exiting zellij
-  #   kill $fish_pid
-  # end
+    zellij attach --create main
 end
