1e00feb878a92cf3b0a9e529867c4ead14587501
Sam Zanca <sam@zanca.dev>
2026-05-20 15:52:35 -0400

    rust sccache is buggy with WSL.

--- home/.config/fish/conf.d/rust.fish
+++ home/.config/fish/conf.d/rust.fish
@@ -1,17 +1,16 @@
-# If this system has rust installed, setup my rust environment how I like it.
+# Cargo fish integration
+if test -f "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
 
-if type -q cargo;
-    # Cargo fish integration
-    if test -f "$HOME/.cargo/env.fish"
-        source "$HOME/.cargo/env.fish"
-    end
+if type -q cargo
+    # TODO try to see if this works ok on macos/linux. On WSL sccache is being strange.
+    # if not grep -q microsoft /proc/version
+    #     if not type -q sccache;
+    #         cargo install sccache;
+    #         set -gx RUSTC_WRAPPER sccache;
+    #     end
+    # end
 
-    # If build cache not installed, install it
-    if not type -q sccache;
-        cargo install sccache;
-    end
-
-    # Set rustc to use build cache
-    set -gx RUSTC_WRAPPER sccache;
-
+    set -gx CARGO_TARGET_DIR "$HOME/.cache/cargo-target"
 end
